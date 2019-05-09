a04bdb1fb8ede7d9543bd0429939bef1f1f1c1ae
Brian D. Conversi <bdconversi@yahoo.com>
2019-05-09 10:37:42 -0500

    Add project files.

---
+++ Whose Turn Is It/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Whose_Turn_Is_It.MvcApplication" Language="C#" %>
---
+++ Whose Turn Is It/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Whose_Turn_Is_It.Startup))]
+namespace Whose_Turn_Is_It
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
